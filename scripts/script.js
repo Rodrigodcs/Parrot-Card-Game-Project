18ea359061fd56f3fea3e028f22e0dacdd8565e2
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-04-06 22:50:45 -0300

    Implemented prompt to ask player number of cards, implemented a function that creates array given the number of cards and one that shuffles it. Starting the implementation of a function that will display the gifs in the order of the shuffled array

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+function numberOfCards(){
+  while((number<4 || number>14) || number%2!=0){
+    number=parseInt(prompt("Com quantas cartas quer jogar? (4 à 14)"));
+  }
+}
+
+function createArray(size){
+  for(let i=0;i<size/2;i++){
+    cardsArray.push(i);
+    cardsArray.push(i);
+  }
+}
+
+function shuffleArray(array){
+  for(let i=0;i<array.length;i++){
+    let j = Math.floor(Math.random()*array.length);
+    let temp = array[i];
+    array[i]=array[j];
+    array[j]=temp;
+  }
+  shuffledArray=array;
+}
+
+function createTable(array){
+  
+}
+
+//console.log(Math.floor(Math.random()*10)); // random inteiro de 0 a 9
+
+// let number = 0;
+// let cardsArray = [];
+// let shuffledArray = [];
+// numberOfCards();
+// createArray(number);
+// shuffleArray(cardsArray);
+// console.log(shuffledArray);
